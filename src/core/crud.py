1a413acd03a538589d1c7a9db0c9ad2a27ac00f1
Malcom Pozo E. <malcompozo@gmail.com>
2024-10-13 22:05:14 -0300

    agrega funciones CRUD para extraer data especifica por numero de factura y actualizacion por montos

--- src/core/crud.py
+++ src/core/crud.py
@@ -26,7 +26,7 @@ def create_invoice(data,table_name):
     cursor.close()
     close_connection(connection)
 
-# Read
+# Read all data
 def read_invoices(table_name):
     """Leer todas las facturas desde la tabla invoices."""
     connection = get_connection()
@@ -48,24 +48,52 @@ def read_invoices(table_name):
     
     return invoices
 
+# Read invoice_number data
+def read_select_invoice(table_name, invoice_number):
+    """Leer todas las facturas desde la tabla invoices."""
+    connection = get_connection()
+    cursor = connection.cursor()
+    
+    # Consulta de selección
+    select_query = f"SELECT * FROM {table_name}where json_value(invoice_data, '$.issuer.invoice_number') = {invoice_number}"
+    
+    # Ejecutar la consulta
+    cursor.execute(select_query)
+    rows = cursor.fetchall()
+    
+    # Procesar los resultados
+    invoices = [{"id": row[0], "data": row[1]} for row in rows]
+    
+    # Cerrar cursor y conexión
+    cursor.close()
+    close_connection(connection)
+    
+    return invoices
+
 # Update
-def update_invoice(invoice_id, new_data,table_name):
+def update_invoice(invoice_number, new_data, table_name):
     """Actualizar una factura existente en la tabla invoices."""
     connection = get_connection()
     cursor = connection.cursor()
     
     # Convertir el nuevo diccionario de Python a JSON
     new_invoice_json = json.dumps(new_data)
+
+    if table_name.upper == 'INVOICES_ISSUED':
+        json_value = "json_value(invoice_data, '$.issuer.invoice_number')"
+    else:
+        json_value = "json_value(invoice_data, '$.invoice_number')"
+        
     
     # Consulta de actualización
     update_query = f"""
-        UPDATE INVOICES_ISSUED
-        SET {table_name} = :new_invoice_data
-        WHERE id = :invoice_id
+        UPDATE {table_name} 
+        SET INVOICE_DATA = :new_invoice_data
+        WHERE {json_value} = :invoice_number
     """
     
     # Ejecutar la actualización
-    cursor.execute(update_query, new_invoice_data=new_invoice_json, invoice_id=invoice_id)
+    cursor.execute(update_query, new_invoice_data=new_invoice_json, invoice_id=invoice_number)
     
     # Confirmar los cambios
     connection.commit()
@@ -73,6 +101,39 @@ def update_invoice(invoice_id, new_data,table_name):
     # Cerrar cursor y conexión
     cursor.close()
     close_connection(connection)
+
+# Update para columnas subtotal, tax, y total, parametros: numero de factura, nuevo subtotal, nuevo tax, nuevo total, tabla a modificar
+def update_selected_invoice(invoice_number, new_subtotal, new_tax, new_total, table_name):
+    """Actualizar campos específicos de una factura existente en la tabla especificada."""
+    connection = get_connection()
+    cursor = connection.cursor()
+    
+    if table_name.upper() == 'INVOICES_ISSUED':
+        invoice_number_json_path = '$.issuer.invoice_number'
+    else:
+        invoice_number_json_path = '$.invoice_number'
+        
+    # Consulta de actualización utilizando JSON_TRANSFORM
+    update_query = f"""
+        UPDATE {table_name}
+        SET invoice_data = JSON_TRANSFORM(invoice_data,
+            SET '$.subtotal' = :new_subtotal,
+            SET '$.tax' = :new_tax,
+            SET '$.total' = :new_total
+        )
+        WHERE JSON_VALUE(invoice_data, '{invoice_number_json_path}') = :invoice_number
+    """
+    
+    # Ejecutar la actualización
+    cursor.execute(update_query, new_subtotal=new_subtotal, new_tax=new_tax, new_total=new_total, invoice_number=invoice_number)
+    
+    # Confirmar los cambios
+    connection.commit()
+    
+    # Cerrar cursor y conexión
+    cursor.close()
+    close_connection(connection)
+
 
 # Delete
 def delete_invoice(invoice_id,table_name):
